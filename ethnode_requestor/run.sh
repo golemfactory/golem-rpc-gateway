c38c10530236caaeace2da550121698d4e4d1016
scx1332 <sieciech@gmail.com>
2022-07-21 15:24:09 +0200

    Run by default 2 instances

--- ethnode_requestor/run.sh
+++ ethnode_requestor/run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 export PATH=".:$PATH"
 
-poetry run python main.py --check-for-yagna true --subnet-tag bor_proxy_subnet
+poetry run python main.py --check-for-yagna true --subnet-tag ${SUBNET:-bor_proxy_subnet} --num-instances ${NUM_INSTANCES:-2}
 
 
