a95426c19418ba85caa59a3e78c91ffe4a497ff1
scx1332 <sieciech@gmail.com>
2022-09-04 13:09:27 +0200

    Adding postgres to db possibilities

--- ethnode_requestor/db.py
+++ ethnode_requestor/db.py
@@ -1,5 +1,14 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+def get_postgres_connection_string():
+    user = os.environ.get("POSTGRES_USER", "user")
+    password = os.environ.get("POSTGRES_PASSWORD", "ICmDzTGZri")
+    host = os.environ.get("POSTGRES_HOST", "localhost")
+    port = os.environ.get("POSTGRES_PORT", "5432")
+    db = os.environ.get("POSTGRES_DB", "gateway")
+    return f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
 
 
 def get_db_engine():
@@ -8,9 +17,10 @@ def get_db_engine():
         conn_string = os.environ.get('SQLITE_CONN_STRING', 'sqlite:///ethnode.sqlite')
         engine = create_engine(conn_string, echo=False, future=True)
     elif db_type == "postgres":
-        conn_string = os.environ.get('POSTGRES_CONN_STRING',
-                                     'postgresql+psycopg2://user:ICmDzTGZri@localhost/folder-monitor')
+        conn_string = os.environ.get('POSTGRES_CONN_STRING', get_postgres_connection_string())
         engine = create_engine(conn_string, echo=False, future=True)
+        if not database_exists(engine.url):
+            create_database(engine.url)
     else:
         raise Exception(f"Unknown database type {db_type}")
     return engine
