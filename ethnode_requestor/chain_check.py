8e4fbc194f8afaa255d0d933aec5f3ddb2332ef6
scx1332 <sieciech@gmail.com>
2022-09-09 12:18:03 +0200

    Added async clients

--- ethnode_requestor/chain_check.py
+++ ethnode_requestor/chain_check.py
@@ -2,6 +2,22 @@ import aiohttp
 import requests
 import datetime
 
+async def get_chain_id(url):
+    post_data = {
+        "jsonrpc": "2.0",
+        "method": "web3_clientVersion",
+        "params": [],
+        "id": 1
+    }
+    # todo: change to async api
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=post_data, timeout=2) as result:
+            if result.status == 200:
+                block = await result.json()
+
+    return block["result"]
+
+
 async def get_block_info(url):
     post_data = {
         "jsonrpc": "2.0",
@@ -19,6 +35,8 @@ async def get_block_info(url):
 
 
 async def get_short_block_info(url):
+    chain_id = await get_chain_id(url)
+
     block = await get_block_info(url)
     block_short_info = {
         "timestamp": block["timestamp"],
--- ethnode_requestor/db.py
+++ ethnode_requestor/db.py
@@ -5,38 +5,35 @@ from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 
-def get_postgres_connection_string():
+def get_postgres_connection_string(is_async=False):
     user = os.environ.get("POSTGRES_USER", "user")
     password = os.environ.get("POSTGRES_PASSWORD", "ICmDzTGZri")
     host = os.environ.get("POSTGRES_HOST", "localhost")
     port = os.environ.get("POSTGRES_PORT", "5432")
     db = os.environ.get("POSTGRES_DB", "gateway")
-    return f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
-
-
-def get_db_engine():
-    db_type = os.environ.get("MONITOR_DB_ENGINE", "sqlite")
-    if db_type == "sqlite":
-        conn_string = os.environ.get('SQLITE_CONN_STRING', 'sqlite:///ethnode.sqlite')
-        engine = create_engine(conn_string, echo=False, future=True)
-    elif db_type == "postgres":
-        conn_string = os.environ.get('POSTGRES_CONN_STRING', get_postgres_connection_string())
-        engine = create_engine(conn_string, echo=False, future=True)
-        if not database_exists(engine.url):
-            create_database(engine.url)
+    if is_async:
+        return f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{db}"
     else:
-        raise Exception(f"Unknown database type {db_type}")
-    return engine
+        return f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
 
 
-def get_async_db_engine():
+def get_db_engine(is_async=False):
     db_type = os.environ.get("MONITOR_DB_ENGINE", "sqlite")
     if db_type == "sqlite":
-        conn_string = os.environ.get('SQLITE_CONN_STRING', 'sqlite+aiosqlite:///ethnode.sqlite')
-        engine = create_async_engine(conn_string, echo=False, future=True)
+        if is_async:
+            conn_string = os.environ.get('SQLITE_CONN_STRING', 'sqlite+aiosqlite:///ethnode.sqlite')
+            engine = create_async_engine(conn_string, echo=False, future=True)
+        else:
+            conn_string = os.environ.get('SQLITE_CONN_STRING', 'sqlite:///ethnode.sqlite')
+            engine = create_engine(conn_string, echo=False, future=True)
+
     elif db_type == "postgres":
         conn_string = os.environ.get('POSTGRES_CONN_STRING', get_postgres_connection_string())
-        engine = create_async_engine(conn_string, echo=False, future=True)
+        if is_async:
+            engine = create_async_engine(conn_string, echo=False, future=True)
+        else:
+            engine = create_async_engine(conn_string, echo=False, future=True)
+
         if not database_exists(engine.url):
             create_database(engine.url)
     else:
@@ -45,7 +42,7 @@ def get_async_db_engine():
 
 
 db_engine = get_db_engine()
-db_async_engine = get_async_db_engine()
+db_async_engine = get_db_engine(is_async=True)
 async_session = sessionmaker(
     db_async_engine, expire_on_commit=False, class_=AsyncSession
 )
